f5be5e2e23c9ae91b524da15a01b2ae8611a99b2
Alejandro Sedano <alejandro@sistrategia.com>
2018-08-21 10:52:09 -0500

    adds mobile version

---
+++ src/reusableCodeElements/navbarDropdownJS.js
@@ -0,0 +1,22 @@
+function openMenu() {
+ document.getElementById("dropDownWrap").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+    if (!event.target.matches('.dropdownB')) {
+
+        setTimeout(function(){ 
+            
+        var dropdowns = document.getElementsByClassName("dropDownWrap");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+          var openDropdown = dropdowns[i];
+          if (openDropdown.classList.contains('show')) {
+            openDropdown.classList.remove('show');
+          }
+        }
+
+         }, 200);
+  
+      }
+    }
